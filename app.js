e512b28ca50e55fdfe3078ec696e67c8eb3fc4e4
hishanto <husain.imambd@gmail.com>
2022-11-23 13:50:39 +0600

    Day 2: String Concatination

--- app.js
+++ app.js
@@ -109,11 +109,17 @@
 // Math.cos(60)
 //    Strings
 let space = ' '           // an empty space string
-let firstName = 'Asabeneh'
-let lastName = 'Yetayeh'
-let country = 'Finland'
-let city = 'Helsinki'
+let firstName = 'Hus'
+let lastName = 'faaa'
+let country = 'mal'
+let city = 'Dha'
 let language = 'JavaScript'
 let job = 'teacher'
-let quote = "The saying,'Seeing is Believing' is not correct in 2020."
-let quotWithBackTick = `The saying,'Seeing is Believing' is not correct in 2020.`
+let quote = "The saying,'Seeing is Believing' is not correct in 2022."
+let quotWithBackTick = `The saying,'Seeing is Believing' is not correct in 2022.`
+age = 300
+//String Concatenation
+let fullName = firstName + space + lastName; // concatenation, merging two string together.
+let personInfoOne = fullName + '. I am ' + age + '. I live in ' + country; // ES5 string addition
+console.log(fullName);
+console.log(personInfoOne);
