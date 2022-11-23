6a56c1649044aa5f62869cc69055e95ebf40327b
hishanto <husain.imambd@gmail.com>
2022-11-23 13:45:14 +0600

    Day 2: Strings basic

--- app.js
+++ app.js
@@ -52,58 +52,68 @@
 //     const bodyTemp = 37      // oC average human body temperature, which is a constant
     
 //     console.log(age, gravity, mass, PI, boilingPoint, bodyTemp)
-const PI = Math.PI
+// const PI = Math.PI
 
-console.log(PI)                            // 3.141592653589793
+// console.log(PI)                            // 3.141592653589793
 
-// Rounding to the closest number
-// if above .5 up if less 0.5 down rounding
+// // Rounding to the closest number
+// // if above .5 up if less 0.5 down rounding
 
-console.log(Math.round(PI))                // 3 to round values to the nearest number
+// console.log(Math.round(PI))                // 3 to round values to the nearest number
 
-console.log(Math.round(9.81))              // 10
+// console.log(Math.round(9.81))              // 10
 
-console.log(Math.floor(PI))                // 3 rounding down
+// console.log(Math.floor(PI))                // 3 rounding down
 
-console.log(Math.ceil(PI))                 // 4 rounding up
+// console.log(Math.ceil(PI))                 // 4 rounding up
 
-console.log(Math.min(-5, 3, 20, 4, 5, 10)) // -5, returns the minimum value
+// console.log(Math.min(-5, 3, 20, 4, 5, 10)) // -5, returns the minimum value
 
-console.log(Math.max(-5, 3, 20, 4, 5, 10)) // 20, returns the maximum value
+// console.log(Math.max(-5, 3, 20, 4, 5, 10)) // 20, returns the maximum value
 
-const randNum = Math.random() // creates random number between 0 to 0.999999
-console.log(randNum)
+// const randNum = Math.random() // creates random number between 0 to 0.999999
+// console.log(randNum)
 
-// Let us  create random number between 0 to 10
+// // Let us  create random number between 0 to 10
 
-const num = Math.floor(Math.random () * 11) // creates random number between 0 and 10
-console.log(num)
+// const num = Math.floor(Math.random () * 11) // creates random number between 0 and 10
+// console.log(num)
 
-//Absolute value
-console.log(Math.abs(-10))      // 10
+// //Absolute value
+// console.log(Math.abs(-10))      // 10
 
-//Square root
-console.log(Math.sqrt(100))     // 10
+// //Square root
+// console.log(Math.sqrt(100))     // 10
 
-console.log(Math.sqrt(2))       // 1.4142135623730951
+// console.log(Math.sqrt(2))       // 1.4142135623730951
 
-// Power
-console.log(Math.pow(3, 2))     // 9
+// // Power
+// console.log(Math.pow(3, 2))     // 9
 
-console.log(Math.E)             // 2.718
+// console.log(Math.E)             // 2.718
 
-// Logarithm
-// Returns the natural logarithm with base E of x, Math.log(x)
-console.log(Math.log(2))        // 0.6931471805599453
-console.log(Math.log(10))       // 2.302585092994046
+// // Logarithm
+// // Returns the natural logarithm with base E of x, Math.log(x)
+// console.log(Math.log(2))        // 0.6931471805599453
+// console.log(Math.log(10))       // 2.302585092994046
 
-// Returns the natural logarithm of 2 and 10 respectively
-console.log(Math.LN2)           // 0.6931471805599453
-console.log(Math.LN10)          // 2.302585092994046
+// // Returns the natural logarithm of 2 and 10 respectively
+// console.log(Math.LN2)           // 0.6931471805599453
+// console.log(Math.LN10)          // 2.302585092994046
 
-// Trigonometry
-Math.sin(0)
-Math.sin(60)
+// // Trigonometry
+// Math.sin(0)
+// Math.sin(60)
 
-Math.cos(0)
-Math.cos(60)
+// Math.cos(0)
+// Math.cos(60)
+//    Strings
+let space = ' '           // an empty space string
+let firstName = 'Asabeneh'
+let lastName = 'Yetayeh'
+let country = 'Finland'
+let city = 'Helsinki'
+let language = 'JavaScript'
+let job = 'teacher'
+let quote = "The saying,'Seeing is Believing' is not correct in 2020."
+let quotWithBackTick = `The saying,'Seeing is Believing' is not correct in 2020.`
