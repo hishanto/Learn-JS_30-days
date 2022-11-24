98beb0fb8f010aff5dbbdcc76b2d35985e399ab1
hishanto <husain.imambd@gmail.com>
2022-11-24 15:11:55 +0600

    Day 2: String Method

--- app.js
+++ app.js
@@ -108,18 +108,88 @@
 // Math.cos(0)
 // Math.cos(60)
 //    Strings
-let space = ' '           // an empty space string
-let firstName = 'Hus'
-let lastName = 'faaa'
-let country = 'mal'
-let city = 'Dha'
-let language = 'JavaScript'
-let job = 'teacher'
-let quote = "The saying,'Seeing is Believing' is not correct in 2022."
-let quotWithBackTick = `The saying,'Seeing is Believing' is not correct in 2022.`
-age = 300
-//String Concatenation
-let fullName = firstName + space + lastName; // concatenation, merging two string together.
-let personInfoOne = fullName + '. I am ' + age + '. I live in ' + country; // ES5 string addition
-console.log(fullName);
-console.log(personInfoOne);
+// let space = ' '           // an empty space string
+// let firstName = 'Hus'
+// let lastName = 'faaa'
+// let country = 'mal'
+// let city = 'Dha'
+// let language = 'JavaScript'
+// let job = 'teacher'
+// let quote = "The saying,'Seeing is Believing' is not correct in 2022."
+// let quotWithBackTick = `The saying,'Seeing is Believing' is not correct in 2022.`
+// age = 300
+// //String Concatenation
+// let fullName = firstName + space + lastName; // concatenation, merging two string together.
+// let personInfoOne = fullName + '. I am ' + age + '. I live in ' + country; // ES5 string addition
+// console.log(fullName);
+// console.log(personInfoOne);
+// // long literal Strings
+// const paragraph = "My name is Asabeneh Yetayeh. I live in Finland, Helsinki.\
+// I am a teacher and I love teaching. I teach HTML, CSS, JavaScript, React, Redux, \
+// Node.js, Python, Data Analysis and D3.js for anyone who is interested to learn. \
+// In the end of 2019, I was thinking to expand my teaching and to reach \
+// to global audience and I started a Python challenge from November 20 - December 19.\
+// It was one of the most rewarding and inspiring experience.\
+// Now, we are in 2020. I am enjoying preparing the 30DaysOfJavaScript challenge and \
+// I hope you are enjoying too."
+
+console.log(paragraph)
+// \n: new line
+// \t: Tab, means 8 spaces
+// \\: Back slash
+// \': Single quote (')
+// \": Double quote (")
+console.log('I hope everyone is enjoying the 30 Days Of JavaScript challenge.\nDo you ?') // line break
+console.log('Days\tTopics\tExercises')
+console.log('Day 1\t3\t5')
+console.log('Day 2\t3\t5')
+console.log('Day 3\t3\t5')
+console.log('Day 4\t3\t5')
+console.log('This is a backslash  symbol (\\)') // To write a backslash
+console.log('In every programming language it starts with \"Hello, World!\"')
+console.log("In every programming language it starts with \'Hello, World!\'")
+console.log('The saying \'Seeing is Believing\' isn\'t correct in 2020')
+//Template Literals (Template Strings)
+let personInfoThree = `I am ${fullName}. I live in ${city}, ${country}. I am a ${job}. I practice ${language}.`
+console.log(personInfoThree)
+let a = 2
+let b = 3
+console.log(`${a} is greater than ${b}: ${a > b}`)// false
+//      String method
+let js = 'JavaScript'
+console.log(js.length)         // 10
+console.log(firstName.length)  // 8
+
+let stringN = 'JavaScript'
+
+console.log(stringN.toUpperCase())     // JAVASCRIPT
+
+
+
+console.log(firstName.toUpperCase())  // 
+console.log(firstName.toLocaleLowerCase()) 
+console.log(country.toUpperCase())    // 
+
+
+// subtract from string -- substr(index, slices)
+console.log(string.substr(4,6)) 
+
+// subtract from string -- substring(index, end)
+console.log(string.substring(4,10)) 
+console.log(string.substring(4)) 
+
+//trim(): Removes trailing space in the beginning or the end of a string.
+let string = '   30 Days Of JavaScript   '
+console.log(string);
+console.log(string.trim(' ')); // 
+//includes(): It takes a substring argument and it checks if substring argument exists in the string. includes() returns a boolean. If a substring exist in a string, it returns true, otherwise it returns false.
+
+
+console.log(string.includes('Days'))     // true
+console.log(string.includes('days'))     // false - it is case sensitive!
+console.log(string.includes('Script'))   // true
+console.log(string.includes('script'))   // false
+console.log(string.includes('java'))     // false
+console.log(string.includes('Java'))     // true
+
+//
