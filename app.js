03204696ce37ad1dbe7f29c37f175b0216dc0e63
hishanto <husain.imambd@gmail.com>
2022-11-28 13:16:11 +0600

    Day 5:lastIndex of, includes, toString

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
 //day 5
-//create array with array constructor and fill with data
-const arr = Array() // creates an an empty array
-console.log(arr)
+//give last index
+const numbers = [1, 2, 3, 4, 5, 3, 1, 2]
 
-const eightXvalues = Array(8).fill('X') // it creates eight element values filled with 'X'
-console.log(eightXvalues) // ['X', 'X','X','X','X','X','X','X']
+console.log(numbers.lastIndexOf(2)) // 7
+console.log(numbers.lastIndexOf(0)) // -1
+console.log(numbers.lastIndexOf(1)) //  6
+console.log(numbers.lastIndexOf(4)) //  3
+console.log(numbers.lastIndexOf(6)) // -1
+//boolean inclues
+console.log(numbers.includes(5)) // true
+console.log(numbers.includes(0)) // false
+console.log(numbers.includes(1)) // true
+console.log(numbers.includes(6)) // false
 
-const eight0values = Array(8).fill(0) // it creates eight element values filled with '0'
-console.log(eight0values) // [0, 0, 0, 0, 0, 0, 0, 0]
+//array.isarray is to checkif it array
+console.log(numbers.includes(5)) // true
+console.log(numbers.includes(0)) // false
+console.log(numbers.includes(1)) // true
+console.log(numbers.includes(6)) // false
 
-const four4values = Array(4).fill(4) // it creates 4 element values filled with '4'
-console.log(four4values) // [4, 4, 4, 4]
-//concat
-const firstList = [1, 2, 3]
-const secondList = [4, 5, 6]
-const thirdList = firstList.concat(secondList)
+// covert array to string
 
-console.log(thirdList) // [1, 2, 3, 4, 5, 6]
-//indexOf:To check if an item exist in an array. If it exists it returns the index else it returns -1.
-const numbers = [1, 2, 3, 4, 5]
+console.log(numbers.toString()) // 1,2,3,4,5
 
-console.log(numbers.indexOf(5)) // -> 4
-console.log(numbers.indexOf(0)) // -> -1
-console.log(numbers.indexOf(1)) // -> 0
-console.log(numbers.indexOf(6)) // -> -1
+const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+console.log(names.toString()) // Asabeneh,Mathias,Elias,Brook
