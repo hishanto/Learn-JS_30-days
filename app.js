4c92ca9fae3b03600243e88aed01896aa057a322
hishanto <husain.imambd@gmail.com>
2022-11-28 12:54:58 +0600

    Day 5: array - Indexof, concat, fill

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
 //day 5
-// This the most recommended way to create an empty list
+//create array with array constructor and fill with data
+const arr = Array() // creates an an empty array
+console.log(arr)
+
+const eightXvalues = Array(8).fill('X') // it creates eight element values filled with 'X'
+console.log(eightXvalues) // ['X', 'X','X','X','X','X','X','X']
+
+const eight0values = Array(8).fill(0) // it creates eight element values filled with '0'
+console.log(eight0values) // [0, 0, 0, 0, 0, 0, 0, 0]
+
+const four4values = Array(4).fill(4) // it creates 4 element values filled with '4'
+console.log(four4values) // [4, 4, 4, 4]
+//concat
+const firstList = [1, 2, 3]
+const secondList = [4, 5, 6]
+const thirdList = firstList.concat(secondList)
+
+console.log(thirdList) // [1, 2, 3, 4, 5, 6]
+//indexOf:To check if an item exist in an array. If it exists it returns the index else it returns -1.
 const numbers = [1, 2, 3, 4, 5]
-numbers[0] = 10      // changing 1 at index 0 to 10
-numbers[1] = 20      // changing  2 at index 1 to 20
 
-console.log(numbers) // [10, 20, 3, 4, 5]
-
-const countries = [
-  'Albania',
-  'Bolivia',
-  'Canada',
-  'Denmark',
-  'Ethiopia',
-  'Finland',
-  'Germany',
-  'Hungary',
-  'Ireland',
-  'Japan',
-  'Kenya'
-]
-
-countries[0] = 'Afghanistan'  // Replacing Albania by Afghanistan
-let lastIndex = countries.length - 1
-countries[lastIndex] = 'Korea' // Replacing Kenya by Korea
-
-console.log(countries)
+console.log(numbers.indexOf(5)) // -> 4
+console.log(numbers.indexOf(0)) // -> -1
+console.log(numbers.indexOf(1)) // -> 0
+console.log(numbers.indexOf(6)) // -> -1
