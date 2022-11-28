b75eaf1c63ac390d3dd65e53d86d073ec8189e6e
hishanto <husain.imambd@gmail.com>
2022-11-28 12:19:54 +0600

    Day 5: array is mutable

--- app.js
+++ app.js
@@ -1,58 +1,27 @@
 //day 5
 // This the most recommended way to create an empty list
-const arr = []
-console.log(arr)
+const numbers = [1, 2, 3, 4, 5]
+numbers[0] = 10      // changing 1 at index 0 to 10
+numbers[1] = 20      // changing  2 at index 1 to 20
 
-//this is array with value
-const numbers = [0, 3.14, 9.81, 37, 98.6, 100] // array of numbers
-const fruits = ['banana', 'orange', 'mango', 'lemon'] // array of strings, fruits
-const vegetables = ['Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot'] // array of strings, vegetables
-const animalProducts = ['milk', 'meat', 'butter', 'yoghurt'] // array of strings, products
-const webTechs = ['HTML', 'CSS', 'JS', 'React', 'Redux', 'Node', 'MongDB'] // array of web technologies
-const countries = ['Finland', 'Denmark', 'Sweden', 'Norway', 'Iceland'] // array of strings, countries
+console.log(numbers) // [10, 20, 3, 4, 5]
 
-// Print the array and its length
+const countries = [
+  'Albania',
+  'Bolivia',
+  'Canada',
+  'Denmark',
+  'Ethiopia',
+  'Finland',
+  'Germany',
+  'Hungary',
+  'Ireland',
+  'Japan',
+  'Kenya'
+]
 
-console.log('Numbers:', numbers)
-console.log('Number of numbers:', numbers.length)
+countries[0] = 'Afghanistan'  // Replacing Albania by Afghanistan
+let lastIndex = countries.length - 1
+countries[lastIndex] = 'Korea' // Replacing Kenya by Korea
 
-console.log('Fruits:', fruits)
-console.log('Number of fruits:', fruits.length)
-
-console.log('Vegetables:', vegetables)
-console.log('Number of vegetables:', vegetables.length)
-
-console.log('Animal products:', animalProducts)
-console.log('Number of animal products:', animalProducts.length)
-
-console.log('Web technologies:', webTechs)
-console.log('Number of web technologies:', webTechs.length)
-
-console.log('Countries:', countries)
-console.log('Number of countries:', countries.length)
-
-// array with different data type
-const arrr = [
-    'Asabeneh',
-    250,
-    true,
-    { country: 'Finland', city: 'Helsinki' },
-    { skills: ['HTML', 'CSS', 'JS', 'React', 'Python'] }
-] // arr containing different data types
-console.log(arr)
-// creating array with string split
-
-let js = 'JavaScript'
-const charsInJavaScript = js.split('')
-
-console.log(charsInJavaScript) // ["J", "a", "v", "a", "S", "c", "r", "i", "p", "t"]
-
-let companiesString = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
-const companies = companiesString.split(',')
-
-console.log(companies) // ["Facebook", " Google", " Microsoft", " Apple", " IBM", " Oracle", " Amazon"]
-let txt =
-  'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.'
-const words = txt.split(' ')
-
-console.log(words)
+console.log(countries)
