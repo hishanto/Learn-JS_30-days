3b7cfcf89e561a3967474aad6b9d5be5773e5f4c
hishanto <husain.imambd@gmail.com>
2022-12-02 21:13:29 +0600

    Day 6: for of loops

--- app.js
+++ app.js
@@ -1,22 +1,51 @@
 //day 6
-//for loops 
-for(let i = 0; i <= 5; i++){
-    console.log(`${i} * ${i} = ${i * i}`)
-  }
-const countries = ['Finland', 'Sweden', 'Denmark', 'Norway', 'Iceland']
-const newArr = []
-for(let i = 0; i < countries.length; i++){
-  newArr.push(countries[i].toUpperCase())
+//for  of loops 
+const numbers = [1, 2, 3, 4, 5]
+
+for (const num of numbers) {
+  console.log(num)
 }
 
-//while loop
-let i = 0
-while (i <= 5) {
-  console.log(i)
-  i++
+// 1 2 3 4 5
+
+for (const num of numbers) {
+  console.log(num * num)
 }
-do {
-    console.log(i)
-    i++
-  } while (i <= 5)
-  
+
+// 1 4 9 16 25
+
+// adding all the numbers in the array
+let sum = 0
+for (const num of numbers) {
+  sum = sum + num  
+	// can be also shorten like this, sum += num
+  // after this we will use the shorter synthax(+=, -=, *=, /= etc)
+}
+console.log(sum) // 15
+
+const webTechs = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React',
+  'Redux',
+  'Node',
+  'MongoDB'
+]
+
+for (const tech of webTechs) {
+  console.log(tech.toUpperCase())
+}
+
+// HTML CSS JAVASCRIPT REACT NODE MONGODB
+
+for (const tech of webTechs) {
+  console.log(tech[0]) // get only the first letter of each element,  H C J R N M
+}
+const countries = ['Finland', 'Sweden', 'Norway', 'Denmark', 'Iceland']
+const newArr = []
+for(const country of countries){
+  newArr.push(country.toUpperCase())
+}
+
+console.log(newArr)
