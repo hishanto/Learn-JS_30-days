5be3a3ffa51eba7b1975c1cccba0cac91cd3ff9f
hishanto <husain.imambd@gmail.com>
2022-12-02 20:50:47 +0600

    Day 6: for loops and while loops

--- app.js
+++ app.js
@@ -1,27 +1,22 @@
-//day 5
-//give last index
-const numbers = [1, 2, 3, 4, 5, 3, 1, 2]
+//day 6
+//for loops 
+for(let i = 0; i <= 5; i++){
+    console.log(`${i} * ${i} = ${i * i}`)
+  }
+const countries = ['Finland', 'Sweden', 'Denmark', 'Norway', 'Iceland']
+const newArr = []
+for(let i = 0; i < countries.length; i++){
+  newArr.push(countries[i].toUpperCase())
+}
 
-console.log(numbers.lastIndexOf(2)) // 7
-console.log(numbers.lastIndexOf(0)) // -1
-console.log(numbers.lastIndexOf(1)) //  6
-console.log(numbers.lastIndexOf(4)) //  3
-console.log(numbers.lastIndexOf(6)) // -1
-//boolean inclues
-console.log(numbers.includes(5)) // true
-console.log(numbers.includes(0)) // false
-console.log(numbers.includes(1)) // true
-console.log(numbers.includes(6)) // false
-
-//array.isarray is to checkif it array
-console.log(numbers.includes(5)) // true
-console.log(numbers.includes(0)) // false
-console.log(numbers.includes(1)) // true
-console.log(numbers.includes(6)) // false
-
-// covert array to string
-
-console.log(numbers.toString()) // 1,2,3,4,5
-
-const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
-console.log(names.toString()) // Asabeneh,Mathias,Elias,Brook
+//while loop
+let i = 0
+while (i <= 5) {
+  console.log(i)
+  i++
+}
+do {
+    console.log(i)
+    i++
+  } while (i <= 5)
+  
