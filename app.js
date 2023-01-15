83dbd0afea1ae960af8117aebc3eb2e79ac18664
hishanto <husain.imambd@gmail.com>
2023-01-15 23:15:50 +0600

    Day 8: Object method => Object.assign

--- app.js
+++ app.js
@@ -1,42 +1,24 @@
 //day 8
-//setting new key from an object
+//Object method => Object.assign
 const person = {
   firstName: 'Asabeneh',
-  lastName: 'Yetayeh',
   age: 250,
   country: 'Finland',
-  city: 'Helsinki',
-  skills: [
-    'HTML',
-    'CSS',
-    'JavaScript',
-    'React',
-    'Node',
-    'MongoDB',
-    'Python',
-    'D3.js'
-  ],
-  getFullName: function() {
-    return `${this.firstName} ${this.lastName}`
+  city:'Helsinki',
+  skills: ['HTML', 'CSS', 'JS'],
+  title: 'teacher',
+  address: {
+    street: 'Heitamienkatu 16',
+    pobox: 2002,
+    city: 'Helsinki'
+  },
+  getPersonInfo: function() {
+    return `I am ${this.firstName} and I live in ${this.city}, ${this.country}. I am ${this.age}.`
   }
 }
-person.nationality = 'Ethiopian'
-person.country = 'Finland'
-person.title = 'teacher'
-person.skills.push('Meteor')
-person.skills.push('SasS')
-person.isMarried = true
 
-person.getPersonInfo = function() {
-  let skillsWithoutLastSkill = this.skills
-    .splice(0, this.skills.length - 1)
-    .join(', ')
-  let lastSkill = this.skills.splice(this.skills.length - 1)[0]
+//Object methods: Object.assign, Object.keys, Object.values, Object.entries
+//hasOwnProperty
 
-  let skills = `${skillsWithoutLastSkill}, and ${lastSkill}`
-  let fullName = this.getFullName()
-  let statement = `${fullName} is a ${this.title}.\nHe lives in ${this.country}.\nHe teaches ${skills}.`
-  return statement
-}
-console.log(person)
-console.log(person.getPersonInfo())
+const copyPerson = Object.assign({}, person)
+console.log(copyPerson)
